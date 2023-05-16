2c898017bad82201ee4281e5a13c166da91b9129
nonsodaniel <nonsodaniel07@gmail.com>
2023-05-16 17:43:46 +0100

    Added dummy data to generate more list

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <header className="header" data-testid="header">
       <div className="header-wrap text-center">
-        <h3 className="font-20">Virtualized list</h3>
+        <h1 className="title">Virtualized list</h1>
         <div className="btn-wrap">
           <button className="btn btn-add">Add new item</button>
         </div>
--- src/components/lists/Lists.tsx
+++ src/components/lists/Lists.tsx
@@ -2,10 +2,10 @@ import List from "./List";
 import "./lists.scss";
 
 const Lists = () => {
-  const arr = Array.from(Array(5));
+  const arr = Array.from(Array(100));
   return (
     <div className="lists-wrap" data-testid="lists-wrap">
-      <div className="list">
+      <div className="list card">
         {arr.map((o) => (
           <List key={Math.random()} />
         ))}
